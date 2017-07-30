9b16d183e71b50147de6f2ce5c3bb18313a72267
Miao Jiang <jiangfriend@gmail.com>
2017-07-30 17:16:11 +0800

    colorize test output

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 trap 'rm actual.txt' EXIT
 go run addrgen.go test 1 50 > actual.txt
 diff actual.txt expected.txt || {
-	echo "test failed"
+	echo -e "\033[31mtest failed\033[0m"
 	exit 1
 }
+echo -e "\033[32mtest passed\033[0m"
